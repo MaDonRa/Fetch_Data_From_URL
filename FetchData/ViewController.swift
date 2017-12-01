5661afe5372021c65379a001f03f4a45811ea7fc
Sakkaphong Luaengvilai <MaDonRa@Sakkaphongs-MacBook-Pro.local>
2017-12-01 15:14:31 +0700

    Initial Commit

---
+++ FetchData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FetchData
+//
+//  Created by Sakkaphong Luaengvilai on 12/1/2560 BE.
+//  Copyright © 2560 MaDonRa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
